bb6cfab72e5bdd22fecfd5076d0f825e122a492e
AJITHKUMAR A <ajithkumarji1010@gmail.com>
2021-05-03 10:10:56 +0530

    Flat Redux

---
+++ src/containers/modules/flat/FlatComponent.js
@@ -0,0 +1,12 @@
+import react,{Component} from 'react';
+
+function FlatComponent()
+{
+    
+        return(
+            <>
+            <h1>Flat Component</h1>
+            </>
+        );
+}
+export default FlatComponent
---
+++ src/redux/constants/FlatActionTypes.js
@@ -0,0 +1,10 @@
+export const FlatActionTypes = {
+   
+    ADD_FLAT : "ADD_FLAT",
+    UPDATE_FLAT : "UPDATE_FLAT",
+    DELETE_FLAT : "DELETE_FLAT",
+    GET_FLAT : "GET_FLAT",
+    GET_ALL_FLATS : "GET_ALL_FLATS",
+    GET_FLATS_BY_COST : "GET_FLATS_BY_COST"
+    
+}
