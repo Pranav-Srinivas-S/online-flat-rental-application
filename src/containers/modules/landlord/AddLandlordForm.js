fbd0e45bf417a3f50a871f6ece7faf4f9c4b83ab
Pranav-Srinivas-S <pranavsrinivas2000@gmail.com>
2021-05-07 08:33:55 +0530

    Final Commit

--- src/containers/modules/landlord/AddLandlordForm.js
+++ src/containers/modules/landlord/AddLandlordForm.js
@@ -7,10 +7,6 @@ import Container from '@material-ui/core/Container';
 import Box from '@material-ui/core/Box';
 import LandlordValidation from './LandlordValidation';
 import { withRouter } from "react-router-dom";
-import RadioGroup from '@material-ui/core/RadioGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Radio from '@material-ui/core/Radio';
-
 
 class AddLandlordForm extends React.Component {
 
